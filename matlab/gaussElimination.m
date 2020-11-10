c9aed4b4a8b605eb4d6023b4cf31f013445fef17
Hamidreza Norouzi <hamid.r.norouzi@gmail.com>
2020-11-11 01:34:02 +0330

    Gauss elemination and Thomas methods (Matlab)

---
+++ matlab/gaussElimination.m
@@ -0,0 +1,82 @@
+% ---------------- Matlab function ---------------------------
+% Numerical methods course, AUT
+% website: www.cemf.ir
+% Gauss elimination with partial pivoting to find the
+% solution to AX=B 
+% DEFs:
+%inputs:
+%   A: n x n coefficient matrix
+%   B: n x 1 vector of known values
+%output:
+%   X: solution of the set
+%   detA: determinant of A
+function [X, detA] = gaussElimination(A,B, tol)
+
+    %number of rows and columns    
+    [nr, nc] = size(A); 
+    
+    if( nr~= nc)
+        error('Coefficient matrix is not square!');
+    end
+    
+    %number of equations in the set
+    n = length(B);
+    if( n~= nr)
+        error( 'A and B size mismatch');
+    end
+    
+    if( nargin<3)
+        tol = 1.0e-12;
+    end
+    
+    Aug = [A B]; %Augmented matrix
+    detA = 1;
+    
+    %main loop for forward elimination 
+    for k = 1:n-1
+        %first: partial pivoting
+        pElement = abs(Aug(k,k));
+        pRow = k;
+        %locate maximum elements in the rows below the pElements
+        for row = k+1:n
+            if(abs(Aug(row,k)) > pElement)
+                pElement = abs( Aug(row,k));
+                pRow = row;
+            end
+        end
+        
+        %interchanges the rows, if necessary
+        if( pRow ~= k)
+            temp = Aug(k,:);
+            Aug(k,:) = Aug(pRow,:);
+            Aug(pRow,:) = temp;
+            detA = - detA; %change of sign
+        end
+        
+        if(abs(Aug(k,k)) < tol )
+            error('Singular matrix!');
+        end
+        % making elements below the pivot element zero
+        for m = k+1:n
+            Aug(m,:) = Aug(m,:) - (Aug(m,k)/Aug(k,k)*Aug(k,:));
+        end
+        
+    end
+    
+    %the last equation never checked
+    if(abs(Aug(n,n)) < tol )
+        error('Singular matrix!');
+    end
+    
+    X = zeros(n,1);
+    
+    % back substitution
+    X(n) = Aug(n,n+1)/Aug(n,n);
+    detA = detA*Aug(n,n);
+    
+    for k=n-1:-1:1
+        X(k) = (Aug(k,n+1) - dot(Aug(k,k+1:n),X(k+1:n)))/Aug(k,k);
+        detA = detA * Aug(k,k);
+    end
+    
+end 
---
+++ matlab/triDiagonal.m
@@ -0,0 +1,40 @@
+% ---------------- Matlab function ---------------------------
+% Numerical methods course, AUT
+% website: www.cemf.ir
+% Tomas method to solve tri-diagonal set of linear equations 
+% DEFs:
+%inputs:
+%   a: lower diagonal elements [1:n]
+%   b: diagonal elements [1:n]
+%   c: upper diagonal elements [1:n]
+%   d: known coefficients [1:n]
+%output:
+%   X: solution of the set
+
+function X = triDiagonal(a,b, c, d)
+        
+    %number of equations in the set
+    n = length(b);
+    
+    cc = zeros(n,1);
+    dd = zeros(n,1);
+    
+    %forward sweeping
+    cc(1) = c(1)/b(1);
+    for i=2:n-1
+        cc(i) = c(i)/(b(i)-a(i)*cc(i-1));
+    end
+    
+    dd(1) = d(1)/b(1);    
+    for i=2:n
+        dd(i) = (d(i)-a(i)*dd(i-1))/(b(i)-a(i)*cc(i-1));
+    end
+        
+    X = zeros(n,1);
+    % back substitution
+    X(n) = dd(n);
+    for i=n-1:-1:1
+        X(i) = dd(i) - cc(i)*X(i+1);
+    end
+    
+end 
