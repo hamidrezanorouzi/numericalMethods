d7a17fc13441b88ead9fe6504fef8c53344508bb
miladtalebi85 <135502093+miladtalebi85@users.noreply.github.com>
2023-06-13 12:41:39 +0330

    Update Gauss Jordan code

--- matlab/gauss_jordan.m
+++ matlab/gauss_jordan.m
@@ -1,22 +1,33 @@
 function x = gauss_jordan(A,b)
+
 % A: matrix of coefficients
 % b: Vertical vector of constants
 % x: solution vector
 
-A=input('enter matrix of coefficients :');
-b=input('enter vector of Vertical vector :');
+ n=size(b,1);   
+ x=zeros(n,1);  
+ Ab=[A b];
 
-    n=size(b,1);   
-    x=zeros(n,1);  
-    Ab=[A b];
-    
+%First, we must check that no element on the main diameter is zero
+for i=1:n-1
+    for j=i+1:n
+        if (Ab(i,i) == 0) 
+            temp = Ab(i,:);
+            Ab(i,:) = Ab(j,:);
+           Ab(j,:) = temp;
+        end
+    end
+
     for i=1:n      
-        Ab(i,:)=Ab(i,:)./Ab(i,i); 
+        Ab(i,:)=Ab(i,:)/Ab(i,i); 
         for j=1:n  
            if i ~= j  
-               Ab(j,:)=Ab(j,:)-Ab(i,:)*Ab(j,i); 
+               B= Ab(j,i);
+               Ab(j,:)=Ab(j,:)-B*Ab(i,:); 
            end 
         end
     end
+
     x=Ab(:,n+1); 
+
 end
